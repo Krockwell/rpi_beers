cd95d91a0992be9b92918ca4abf29b6785ebbe7b
kevin rockwell <kevinrockwell@gmail.com>
2016-09-03 04:30:56 +0000

    Extended to include any number of probes

--- thermometer.py
+++ thermometer.py
@@ -5,25 +5,33 @@ import time
 import logging
 import logging.handlers
 
+
 os.system('/sbin/modprobe w1-gpio')
 os.system('/sbin/modprobe w1-therm')
 
 base_dir = '/sys/bus/w1/devices/'
-device_folder = glob.glob(base_dir + '28*')[0]
-device_file = device_folder + '/w1_slave'
+
+device_files = []
+probe_count = 0
+
+device_folders = glob.glob(base_dir + '28*')
+for folder in device_folders:
+	probe_count += 1
+	device_files.append(folder + '/w1_slave')
 
 
 def __init__(self):
 	self.hello = "hey duuude"
 
-def read_temp_raw():
-	f = open(device_file, 'r')
+def read_temp_raw(number):
+	f = open(device_files[number], 'r')
 	lines = f.readlines()
 	f.close()
 	return lines
 
-def read_temp():
-	lines = read_temp_raw()
+def read_temp(number):
+	lines = read_temp_raw(number)
+
 	while lines[0].strip()[-3:] != 'YES':
 		time.sleep(0.2)
 		lines = read_temp_raw()
@@ -35,21 +43,24 @@ def read_temp():
 		return temp_c, temp_f
 
 #Setup the Logger
-logger = logging.getLogger('Probe_1')
-logger.setLevel(logging.DEBUG)
+logger = []
+for probe_number in range(probe_count):
+	logger.append(logging.getLogger('Probe' + str(probe_number)))
+	logger[probe_number].setLevel(logging.DEBUG)
 
 console_handler = logging.FileHandler(filename='temperature.log',mode='a')
 console_handler.setLevel(logging.DEBUG)
 
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(message)s')
+formatter = logging.Formatter('%(asctime)s, %(message)s')
 console_handler.setFormatter(formatter)
 
-logger.addHandler(console_handler)
-
+for probe_number in range(probe_count):
+	logger[probe_number].addHandler(console_handler)
 
 
 #Writes the temperature to a file every 2 seconds
 while True:
-	logger.info('%f', read_temp()[1])
+	for probe_number in range(probe_count):
+		logger[probe_number].info('Probe %s: %f', probe_number, read_temp(probe_number)[1])
 	time.sleep(60)
 
